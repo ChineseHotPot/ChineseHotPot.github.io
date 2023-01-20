ee5912d5fcb38ebf8176f35827a7db0068f8ddc8
xuejiaxing-0 <xuejiaxing029@foxmail.com>
2023-01-20 09:53:44 +0800

    fix error

---
+++ nmc/src/settings.72802.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"0fc18",main:"4e292"}};
