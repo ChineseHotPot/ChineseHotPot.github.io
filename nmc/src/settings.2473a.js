8058abe106c4753eb171379faa90bffc177ee44c
xuejiaxing-0 <xuejiaxing029@foxmail.com>
2023-01-28 16:06:13 +0800

    update label

--- nmc/src/settings.2473a.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"aea4a",main:"71007"}};
