840f586e9d984ecec532b11862ad7ed58754d746
xuejiaxing-0 <xuejiaxing029@foxmail.com>
2023-01-31 15:18:55 +0800

    update

---
+++ nmc/src/settings.604d9.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-desktop",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scenes/main.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"aea4a",main:"b0c94"}};
